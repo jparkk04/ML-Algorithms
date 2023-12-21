83531b7ec8d83d132c8778cf623a10dd1338f70a
jparkk04 <parkjus@gmail.com>
2023-12-21 23:22:01 +0800

    Update multivariable_lin_reg.py

--- multivariable_lin_reg.py
+++ multivariable_lin_reg.py
@@ -2,5 +2,9 @@ import numpy as np
 
 class MultivariableLinearRegression:
 
-    def __init__(self):
+    def __init__(self, x, y, learning_rate = 0.01):
+        self.w = np.zeros(len(x[0]))
         self.b = 0
+        self.y = np.array(y)
+        self.x = np.array(x)
+        self.learning_rate = learning_rate
