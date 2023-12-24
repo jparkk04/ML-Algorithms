110c943dec65a847d95bb33914a06afd776cd6ac
jparkk04 <parkjus@gmail.com>
2023-12-24 23:46:12 +0800

    Added cost function

--- multiple_lin_reg.py
+++ multiple_lin_reg.py
@@ -11,3 +11,10 @@ class MultipleLinearRegression:
 
     def calculate(self, xi):
         return np.dot(self.w,xi) + self.b
+    
+    def cost(self):
+        cost = 0
+        for i in range(len(self.x)):
+            cost += (self.y[i] - self.calculate(self.x[i]))**2
+        cost /= len(self.x)
+        return cost
