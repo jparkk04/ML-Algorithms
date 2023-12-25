af10516b254d456fdb2591b98236ae9995dabf2d
jparkk04 <parkjus@gmail.com>
2023-12-25 23:02:59 +0800

    Update multiple_lin_reg.py

--- multiple_lin_reg.py
+++ multiple_lin_reg.py
@@ -18,3 +18,31 @@ class MultipleLinearRegression:
             cost += (self.y[i] - self.calculate(self.x[i]))**2
         cost /= len(self.x)
         return cost
+    
+    def dcostdb(self):
+        d = 0
+        for i in range(len(self.x)):
+            d += 2*(self.y[i] - self.calculate(self.x[i]))
+        d /= len(self.x)
+        return d
+    
+    def dcostdw(self):
+        dlist = np.zeros(len(self.w))
+        for i in range(len(self.w)):
+            d = 0
+            for j in range(len(self.x)):
+                d += 2*(self.y[i] - self.calculate(self.x[i]))*self.x[j][i]
+            dlist[i] = d
+        return dlist
+    
+    def epoch(self):
+        dcdb = self.dcostdb()
+        dcdw = self.dcostdw()
+        self.w -= self.learning_rate*dcdw
+        self.b -= self.learning_rate*dcdb
+
+    def iterate(self, repeat):
+        for _ in range(repeat):
+            self.epoch()
+            print(f'cost {self.cost()}')
+        
