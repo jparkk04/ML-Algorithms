e4768cbb19e746f37bf589a9d4e07692b85ff0ae
jparkk04 <parkjus@gmail.com>
2023-12-31 16:49:45 +0900

    Update logistic_reg.py

--- logistic_reg.py
+++ logistic_reg.py
@@ -1,7 +1,20 @@
 import numpy as np
+import math
 
 class LogisticRegression:
-    def __init__(self, x, y, learning_rate = 0.01):
+    def __init__(self, x, y, decision_boundary = 0.5, learning_rate = 0.01):
+        self.w = np.zeros(len(x[0]))
+        self.b = 0
         self.x = np.array(x)
         self.y = np.array(y)
+        self.decision_boundary = decision_boundary
         self.learning_rate = learning_rate
+
+    def z(self, x):
+        return np.dot(self.w,x) + self.b
+    
+    def sigmoid(self, z):
+        return 1/(1 - math.e**(-z))
+    
+    def f(self,x):
+        return self.sigmoid(self.z(x))
