8acb4ae8fef82d82cd9139c5a4ed96dd84cd6c4a
jparkk04 <parkjus@gmail.com>
2023-12-31 16:36:55 +0900

    Update logistic_reg.py

--- logistic_reg.py
+++ logistic_reg.py
@@ -1,4 +1,7 @@
 import numpy as np
 
 class LogisticRegression:
-    def __init__(self):
+    def __init__(self, x, y, learning_rate = 0.01):
+        self.x = np.array(x)
+        self.y = np.array(y)
+        self.learning_rate = learning_rate
