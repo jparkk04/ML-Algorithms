b229237cc416fc90801dfe036d4dda450d480f13
jparkk04 <parkjus@gmail.com>
2024-01-10 21:09:46 +0900

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -1,5 +1,7 @@
 import numpy as np
 
 class Dense:
-    def __init__(self, neruon_count):
+    def __init__(self, neruon_count, prev_neuron_count):
+        self.neuron_count = neruon_count
         self.b = [np.zeros(neruon_count)]
+        self.prev_neuron_count = prev_neuron_count
